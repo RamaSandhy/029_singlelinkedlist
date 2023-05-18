fca016d0f481e3efae913589ae9761a6bfd93d32
RamaSandhy <rshandy07@gmail.com>
2023-05-18 16:49:50 +0700

    Perubahan ini bertujuan untuk memperbaiki fungsi pencarian di daftar tertaut, memberikan hasil yang benar saat mencari node tertentu berdasarkan nilai 'nim'

--- 029_singlelinkedlist/029_singlelinkedlist.cpp
+++ 029_singlelinkedlist/029_singlelinkedlist.cpp
@@ -50,5 +50,24 @@ void addNode() {
 	previous->next = nodeBaru;
 }
 
+bool serachNode(int nim, Node* current, Node* previous) {
+	previous = START;
+	current = START;
+	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		return false;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 
 
